cfd7740112013b1f84a66365824b963fa422114f
sherazzie <razzierazbuzz@gmail.com>
2015-09-16 16:53:47 +0800

    New Music UI

---
+++ FinalYearProject/MusicView.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Auth;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace FinalYearProject
+{
+    public partial class MusicView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                databind();
+            }
+        }
+
+        protected void databind()
+        {
+            string connstr = "Server=tcp:o18y8i1qfe.database.windows.net,1433;Database=FypjDB;User ID=sherazzie@o18y8i1qfe;Password=Zulamibinsalami21;Trusted_Connection=False;Encrypt=True;Connection Timeout=30;";
+            SqlConnection conn = new SqlConnection(connstr);
+            string cmdstring = "SELECT AlbumArtPath,ArtistName,Album from MusicFiles ";
+            SqlCommand cmd = new SqlCommand(cmdstring, conn);
+            conn.Open();
+            dl_music.DataSource = cmd.ExecuteReader();
+            dl_music.DataBind();
+            conn.Close();
+
+
+        }
+
+        protected void load_songs(object sender,EventArgs e)
+        {
+            
+        }
+    }
+}
